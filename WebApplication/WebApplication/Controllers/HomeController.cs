5c324116785029cff0f55114ef60851167d391a7
veznwang <292456198@qq.com>
2021-05-28 10:53:17 +0800

    1

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.123321";
 
             return View();
         }
